9731864bb14b3e2950065b66d498621f516afb25
Tan Truong <tanfctk@gmail.com>
2024-04-26 16:47:28 +0700

    change API_DOMAIN

--- quiz-app/src/utils/request.js
+++ quiz-app/src/utils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = "http://localhost:3002/";
+const API_DOMAIN = "https://quiz-db.onrender.com/";
 
 export const get = async (path) => {
   const response = await fetch(API_DOMAIN + path);
